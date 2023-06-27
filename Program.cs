43439a5cf501b500aa2f9bb193dcaa68a62fbe63
SERZ3D <137371650+SERZ3D@users.noreply.github.com>
2023-06-27 15:11:41 -0600

    E5

--- Program.cs
+++ Program.cs
@@ -1,43 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace T2_E1
+
+namespace T2_E5
 {
-    internal class Program
+    class ListaCaracteres
     {
-        
+        private List<char> lista;
+
+        public ListaCaracteres()
+        {
+            lista = new List<char>();
+        }
+
+        public void AgregarCaracteres()
+        {
+            Console.Write("Ingrese la cadena de caracteres: ");
+            string cadena = Console.ReadLine();
+                    
+            Console.Write("Ingrese la posición en la que desea insertar la cadena: ");
+            int posicion = int.Parse(Console.ReadLine());
+
+            if (posicion >= 1 && posicion <= lista.Count + 1)
+            {
+                lista.InsertRange(posicion - 1, cadena.ToCharArray());
+                Console.WriteLine("Cadena insertada correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("Posición inválida. No se pudo insertar la cadena.");
+            }
+        }
+
+        public void EliminarCaracter()
+        {
+            Console.Write("Ingrese la posición del carácter que desea eliminar: ");
+            int posicion = int.Parse(Console.ReadLine());
+
+            if (posicion >= 1 && posicion <= lista.Count)
+            {
+                lista.RemoveAt(posicion - 1);
+                Console.WriteLine("Carácter eliminado correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("Posición inválida. No se pudo eliminar el carácter.");
+            }
+        }
+
+        public void EliminarSublista()
+        {
+            Console.Write("Ingrese la posición de inicio de la sublista: ");
+            int inicio = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese la posición de fin de la sublista: ");
+            int fin = int.Parse(Console.ReadLine());
+
+            if (inicio >= 1 && fin <= lista.Count && inicio <= fin)
+            {
+                lista.RemoveRange(inicio - 1, fin - inicio + 1);
+                Console.WriteLine("Sublista eliminada correctamente.");
+            }
+            else
+            {
+                Console.WriteLine("Posiciones inválidas. No se pudo eliminar la sublista.");
+            }
+        }
+
+        public void ImprimirLista()
+        {
+            Console.WriteLine("Contenido de la lista:");
+            foreach (char caracter in lista)
+            {
+                Console.Write(caracter + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Program
+    {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Ingrese tres números enteros:");
+            ListaCaracteres listaCaracteres = new ListaCaracteres();
 
-            // Leer las tres entradas desde la consola
-            Console.Write("Primer número: ");
-            string input1 = Console.ReadLine();
-
-            Console.Write("Segundo número: ");
-            string input2 = Console.ReadLine();
-
-            Console.Write("Tercer número: ");
-            string input3 = Console.ReadLine();
-
-            // Convertir las cadenas de texto en números enteros utilizando int.Parse()
-            try
+            bool salir = false;
+            while (!salir)
             {
-                int num1 = int.Parse(input1);
-                int num2 = int.Parse(input2);
-                int num3 = int.Parse(input3);           
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Uno o más valores ingresados no son números enteros válidos.");
+                Console.WriteLine("Menú:");
+                Console.WriteLine("1. Agregar caracteres a la lista.");
+                Console.WriteLine("2. Eliminar carácter.");
+                Console.WriteLine("3. Eliminar sublista.");
+                Console.WriteLine("4. Salir");
+                Console.Write("Seleccione una opción: ");
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        listaCaracteres.AgregarCaracteres();
+                        break;
+                    case "2":
+                        listaCaracteres.EliminarCaracter();
+                        break;
+                    case "3":
+                        listaCaracteres.EliminarSublista();
+                        break;
+                    case "4":
+                        salir = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opción inválida. Intente nuevamente.");
+                        break;
+                }
+
+                Console.WriteLine();
             }
 
-            Console.WriteLine("Los 3 numeros son: " + num1 + num2 + num3);
-            Console.WriteLine()
+            listaCaracteres.ImprimirLista();
+            Console.WriteLine("¡Hasta luego!");
         }
     }
 }
+
