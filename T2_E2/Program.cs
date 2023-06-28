1253c6bfd96eb0a474466a81d324ea6cb1ac4d9a
Gerardo Gonzalez <68037783+Tokyo-gg@users.noreply.github.com>
2023-06-28 09:54:05 -0600

    Update Program.cs
    Agregar los comentarios y rescribir el codigo para que sea mas preciso con la edad que calcula, tomando en cuaneta años bisiestos

--- T2_E2/Program.cs
+++ T2_E2/Program.cs
@@ -6,81 +6,70 @@
 
 
 namespace T2_E2
-{
-    class Program
+{ using System;
+
+    public class AgeCalculator //Creamos la clase AgeCalculator que va a calcular la edad 
+    {
+        public static void CalcularEdad(DateTime fechaNacimiento) //El argumento de la funcion es la fecha de nacimiento, usando DateTime como tipo de dato
+        {
+            DateTime now = DateTime.Now; //Establecemos la fecha actual
+            //Hacemos la resta de la fecha actual con la fecha de nacimiento para obtener la edad
+            int años = now.Year - fechaNacimiento.Year;
+            int meses = now.Month - fechaNacimiento.Month;
+            int dias = now.Day - fechaNacimiento.Day;
+            int horas = now.Hour - fechaNacimiento.Hour;
+            int minutos = now.Minute - fechaNacimiento.Minute;
+            int segundos = now.Second - fechaNacimiento.Second;
+
+            if (meses < 0 || (meses == 0 && now.Day < fechaNacimiento.Day)) //Condicion para saber los años que ha vivido
+            {
+                años--;
+                meses += 12;
+            }
+
+            if (dias < 0) //Condicion para saber los meses que ha vivido
+            {
+                meses--;
+                dias += DateTime.DaysInMonth(now.Year, now.Month); //Usamos la funcion DaysInMonth para saber los dias que tiene el mes actual, tomando en cuenta si es año bisiesto o no
+            }
+
+            if (horas < 0) //Condicion para saber los dias que ha vivido
+            {
+                dias--;
+                horas += 24;
+            }
+
+            if (minutos < 0) //Condicion para saber las horas que ha vivido
+            {
+                horas--;
+                minutos += 60;
+            }
+
+            if (segundos < 0) //Condicion para saber los minutos que ha vivido, el restante son los segundos
+            {
+                minutos--;
+                segundos += 60;
+            }
+
+            Console.WriteLine($"Edad: {años} años, {meses} meses, {dias} dias, {horas} horas, {minutos} minutos, {segundos} segundos"); //Mostramos la edad
+        }
+    }
+    internal class Program
     {
         static void Main(string[] args)
         {
-            CalculadoraEdad calculadora = new CalculadoraEdad();
-            calculadora.Ejecutar();
+            int dia, mes, año; //Variables para almacenar la fecha de nacimiento
+            Console.WriteLine("Introduce tu fecha de nacimiento: ");
+            Console.WriteLine("Dia: ");
+            dia = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mes: ");
+            mes = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Año: ");
+            año = Convert.ToInt32(Console.ReadLine());
 
-            Console.ReadLine();
+            DateTime fechaNacimiento = new DateTime(año, mes, dia);//Creamos un objeto de tipo DateTime con la fecha de nacimiento que introdujo el usuario
+            AgeCalculator.CalcularEdad(fechaNacimiento);//Llamamos a la funcion CalcularEdad y le pasamos como argumento la fecha de nacimiento
         }
-    }
-
-    class CalculadoraEdad
-    {
-        private DateTime fechaActual;
-
-        public CalculadoraEdad()
-        {
-            fechaActual = DateTime.Now;
-        }
-
-        public void SolicitarFechaNacimiento()
-        {
-            while (true)
-            {
-                try
-                {
-                    Console.Write("Ingrese su fecha de nacimiento (formato: dd/mm/aaaa): ");
-                    string fechaNacimientoStr = Console.ReadLine();
-                    DateTime fechaNacimiento = DateTime.ParseExact(fechaNacimientoStr, "dd/MM/yyyy", null);
-                    FechaNacimiento = fechaNacimiento;
-                    break;
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Formato de fecha incorrecto. Intente nuevamente.");
-                }
-            }
-        }
-
-        public void CalcularEdad()
-        {
-            TimeSpan edad = fechaActual - FechaNacimiento;
-            EdadAnios = edad.Days / 365;
-            EdadMeses = edad.Days / 30;
-            EdadDias = edad.Days;
-            EdadHoras = edad.TotalHours;
-            EdadMinutos = edad.TotalMinutes;
-            EdadSegundos = edad.TotalSeconds;
-        }
-
-        public void ImprimirEdad()
-        {
-            Console.WriteLine("        Edad");
-            Console.WriteLine("Años: " + EdadAnios);
-            Console.WriteLine("Meses: " + EdadMeses);
-            Console.WriteLine("Días: " + EdadDias);
-            Console.WriteLine("Horas: " + EdadHoras);
-            Console.WriteLine("Minutos: " + EdadMinutos);
-            Console.WriteLine("Segundos: " + EdadSegundos);
-        }
-
-        public void Ejecutar()
-        {
-            SolicitarFechaNacimiento();
-            CalcularEdad();
-            ImprimirEdad();
-        }
-
-        public DateTime FechaNacimiento { get; private set; }
-        public int EdadAnios { get; private set; }
-        public int EdadMeses { get; private set; }
-        public int EdadDias { get; private set; }
-        public double EdadHoras { get; private set; }
-        public double EdadMinutos { get; private set; }
-        public double EdadSegundos { get; private set; }
     }
 }
+
